132a2f82dc0ccafed8f7e523c2f0931aec272b75
NehaAchappa <achappa.neha22@gmail.com>
2025-05-17 21:49:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os 
 os.environ["GOOGLE_API_KEY"]=st.secrets["GOOGLE_API_KEY"]
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage 
 
 # Initialize the model
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
